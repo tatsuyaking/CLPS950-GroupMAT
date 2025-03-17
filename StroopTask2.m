7495e13c25921e6e312241a36b018cb0d5f8e6c9
Alex S <alexander_sayette@brown.edu>
2025-03-17 14:53:35 -0400

    Started designing StroopTaskDemo

--- StroopTask2.m
+++ StroopTask2.m
@@ -28,6 +28,36 @@ grey = white / 2;
 % Open an on screen window using PsychImaging and color it grey.
 [window, windowRect] = PsychImaging('OpenWindow', screenNumber, grey);
 
+% Set the blend funciton for the screen
+Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
+
+% Get the size of the on screen window in pixels
+% For help see: Screen WindowSize?
+[screenXpixels, screenYpixels] = Screen('WindowSize', window);
+
+% Get the centre coordinate of the window in pixels
+% For help see: help RectCenter
+[xCenter, yCenter] = RectCenter(windowRect);
+
+% Draw text in the upper portion of the screen with the default font in red
+Screen('TextSize', window, 70);
+DrawFormattedText(window, 'Hello World', 'center',...
+    screenYpixels * 0.25, [1 0 0]);
+
+% Draw text in the middle of the screen in Courier in white
+Screen('TextSize', window, 80);
+Screen('TextFont', window, 'Courier');
+DrawFormattedText(window, 'Hello World', 'center', 'center', white);
+
+% Draw text in the bottom of the screen in Times in blue
+Screen('TextSize', window, 90);
+Screen('TextFont', window, 'Times');
+DrawFormattedText(window, 'Hello World', 'center',...
+    screenYpixels * 0.75, [0 0 1]);
+
+% Flip to the screen
+Screen('Flip', window);
+
 % Now we have drawn to the screen we wait for a keyboard button press (any
 % key) to terminate the demo.
 KbStrokeWait;
