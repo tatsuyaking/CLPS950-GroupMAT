4dcea5a68edb1e0a285e839a98f13f791ed61e30
tatsuyaking <tatsuya_king@brown.edu>
2025-03-17 22:59:09 -0400

    Added some comments and made it more clear

--- import_data.m
+++ import_data.m
@@ -1,20 +1,24 @@
 
 function import_data
 
-data = readtable ("MAT_example_csv - Attention.csv");
+data = readtable ("MAT_example_csv - Attention.csv"); % import csv into code
 
 disp(data)
 
 
-participant_IDs = unique(data.participant_ID);
+participant_IDs = unique(data.participant_ID); % Extracts number of unique values in participant_ID section
 
-for i = 1:length(participant_IDs)
+for i = 1:length(participant_IDs) 
+%for each participant ID, creates a new table with just that participants data
+
     current_ID = participant_IDs(i);
     
     participant_data = data(data.participant_ID == current_ID, :);
+    %for whatever ID the loop is on, the participant_data variable holds
+    %that participants data
     
-    var_name = ['participant_' num2str(current_ID)]; 
-    assignin('base', var_name, participant_data);  
+    var_name = ['participant_' num2str(current_ID)]; % Makes a unique name for each ID
+    assignin('base', var_name, participant_data); % Saves new table 
 end
 
 
