ebd50a455807058cc5a76c1240c38cdee84a0a61
Alex S <alexander_sayette@brown.edu>
2025-03-21 13:00:40 -0400

    Minor updates and comments

--- StroopTaskDemo.m
+++ StroopTaskDemo.m
@@ -65,23 +65,23 @@ halfwayDownY = centerY + (screenYpixels / 4);
 halfwayUpY = centerY - (screenYpixels / 4);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-%                            My added images
+%                            Showing keyboard image
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-imageFile = 'keyboard.jpg';  % Specify the path to JPG file 
+imageFile = 'keyboard.jpg';  % path to JPG file 
 
 if exist(imageFile, 'file')
-    img = imread(imageFile);  % Read the image
+    img = imread(imageFile);  % reads image
 else
     error('Image file not found: %s', imageFile);
 end
 
-% Convert the image to texture to allow for image to be shown
+% Converts the image to texture allowing image to be shown
 imageTexture = Screen('MakeTexture', window, img); 
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%                       Timing Information--helps us capture time
+%                       Timing inforration--helps us capture time
 %               information for reaction time 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -99,9 +99,9 @@ waitframes = 1;
 %   "blue"
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Define the keyboard keys that are listened for. We will be using the left
-% and right arrow keys as response keys for the task and the escape key as
-% a exit/reset key
+% Here I specify which keyboard keys will perform specific functions.
+% Left, right, and down arrow keys capture responses, and escape exits
+
 escapeKey = KbName('ESCAPE');
 leftKey = KbName('LeftArrow');
 rightKey = KbName('RightArrow');
@@ -109,49 +109,38 @@ downKey = KbName('DownArrow');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%            Colors in words and RGB--we continued with the demo's
-%           three colors since we appreciated  
+%            Colors (as words and RGB)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%  We are going to use three colors for this demo. Red, Green and blue.
+%  we are using red, green and blue as our color responses
 wordList = {'Red', 'Green', 'Blue'};
 rgbColors = [1 0 0; 0 1 0; 0 0 1];
 
-% Make the matrix which will determine our condition combinations
+% this matrix determines random combinations of the word and the color it's
+% written in. We then duplicate this condition matrix for each trial
 condMatrixBase = [sort(repmat([1 2 3], 1, 3)); repmat([1 2 3], 1, 3)];
 
-% Number of trials per condition. We set this to one for this demo, to give
-% us a total of 9 trials.
-trialsPerCondition = 1;
+condMatrix = repmat(condMatrixBase, 1, 1);
 
-% Duplicate the condition matrix to get the full number of trials
-condMatrix = repmat(condMatrixBase, 1, trialsPerCondition);
+[~, numTrials] = size(condMatrix); %gets size of matrix
 
-% Get the size of the matrix
-[~, numTrials] = size(condMatrix);
-
-% Randomise the conditions
+% randomizes conditions
 shuffler = Shuffle(1:numTrials);
 condMatrixShuffled = condMatrix(:, shuffler);
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%                     Make a response matrix
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% This is a six row matrix the first row will record an ID for the subject,
+% This generates a 6-row response matrix. The first row records an ID for the subject,
 % the second row the word presented (1 = red, 2 = blue, 3 = green),
 % the third row the color the word it written in (same number-to-color key),
 % the fourth row the key they respond with, the fifth row the time they took
 % to make a response and in the final row 1 if they were sucessful and 0 if not.
+
 respMat = nan(6, numTrials);
 
 
-%----------------------------------------------------------------------
-%                       Experimental loop
-%----------------------------------------------------------------------
-
-% Animation loop: we loop for the total number of trials
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                       loop of the experiment
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 for trial = 1:numTrials
 
@@ -163,12 +152,11 @@ for trial = 1:numTrials
     theWord = wordList(wordNum);
     theColor = rgbColors(colorNum, :);
 
-    % Cue to determine whether a response has been made
+    % determines whether response has been made
     respToBeMade = true;
 
     
-    % If this is the first trial we present a start screen and wait for a
-    % key-press
+    % If this is trial one, show a start screen and wait for any key-press
     if trial == 1
         
         DrawFormattedText(window, 'Welcome to the stroop task! \n\n Click the arrow that corresponds with the color of the text (NOT the word written!).',...
@@ -176,70 +164,52 @@ for trial = 1:numTrials
         DrawFormattedText(window, 'Click the LEFT arrow if color shown is RED, DOWN for GREEN, and RIGHT for BLUE! \n\n Press Any Key To Begin',...
             'center', halfwayDownY, black);
    
-        % Draw the image halfway above the center
+        % Draw image halfway above the center
     
         [imgWidth, imgHeight, ~] = size(img);  % Get image dimensions
 
-        % Scale the width by 8 times
-        imgWidth = imgWidth * 8;  % Want to stretch out image to make more readable
+        % Sretch out image to make more readable
+        imgWidth = imgWidth * 8;  % Scale the width by 8 times
 
-        % this positions images horizontally centered and vertically above
-        % the center
+        % Position images horizontally centered and vertically above the center
         imageX = xCenter - imgWidth / 2;  % Center the image horizontally
         imageY = halfwayUpY - imgHeight / 2;  % Position the image halfway above the center
     
-        % Draw the image texture at the calculated position
+        % Draw image texture at the calculated position
         Screen('DrawTexture', window, imageTexture, [], [imageX, imageY, imageX + imgWidth, imageY + imgHeight]);
 
-        % Flip the screen to display everything
+        % Flip screen to display everything
         Screen('Flip', window);
         
         KbStrokeWait;
     end
 
-    % Flip again to sync us to the vertical retrace at the same time as
-    % drawing our fixation point
+    % Progress to the next screen and syncs vertical retrace at same time as
+    % drawing fixation point (the dot that appears between trials)
     Screen('DrawDots', window, [xCenter; yCenter], 10, black, [], 2);
     vbl = Screen('Flip', window);
 
-    % Make a note of the time stamp
-    iStart = vbl;
+    iStart = vbl; % notes time stamp
 
-    % Now we present the isi interval with fixation point minus one frame
-    % because we presented the fixation point once already when getting a
-    % time stamp. We use the waitframes functionality to do this without
-    % the need of a loop
+    % Show isi interval with fixation point minus one frame to help
+    % find reaction time
 
-    % Draw the fixation point
+    % Draws fixation point
     Screen('DrawDots', window, [xCenter; yCenter], 10, black, [], 2);
 
-    % Flip to the screen
+    % Flips to the screen
     vbl = Screen('Flip', window, vbl + ((isiTimeFrames - 1) - 0.5) * ifi);
 
-    % Get the presentation time of the ISI
+    % Get the presentation time of the ISI to help capture reaction time
     iEnd = vbl;
-    isiRecDuration = iEnd - iStart;
 
-    % Now present the word in continuous loops until the person presses a
-    % key to respond. We take a time stamp before and after to calculate
-    % our reaction time. We could do this directly with the vbl time stamps,
-    % but for the purposes of this introductory demo we will use GetSecs.
-    %
-    % The person should be asked to respond to either the written word or
-    % the color the word is written in. They make thier response with the
-    % three arrow key. They should press "Left" for "Red", "Down" for
-    % "Green" and "Right" for "Blue".
-    %
-    % We could also use Keyboard Queues which offer a potentially more
-    % accurate way in which to get keyboard resoponses. See the Keyboiard
-    % Queue version of this demo.
-    %
-    % Additionally, we poll the keyboard each time we update the screen. We
-    % could alternatively flip to the screen once, then loop over purely
-    % the keyboard polling and clear the screen once a button is pressed.
+    % Show word until person presses a key to respond ("Left" for "Red",
+    % "Down" for "Green" and "Right" for "Blue"), taking time stamp before
+    % and after to calculate reaction time.
+
     while respToBeMade == true
 
-        % Draw the word
+        % Draw word
         DrawFormattedText(window, char(theWord), 'center', 'center', theColor);
 
         % Display the keyboard instructions along with trials
@@ -252,22 +222,20 @@ for trial = 1:numTrials
         imgWidth = imgWidth * 6;  % Want to stretch out image to make more readable
         imgHeight = imgHeight / 2;  % Shrink the height by half
 
-        % this positions images horizontally centered and vertically above
-        % the center
+        % Positions images horizontally centered and vertically above the center
         imageX = xCenter - imgWidth / 2;  % Center the image horizontally
         imageY = halfwayUpY + (4*imgHeight);  % Position the image halfway above the center
     
         % Convert the image to grayscale (black and white) for the
-        % experimental loop --see google doc for expanded reason for why I
-        % chose NOT to show color (essentially it makes the Stroop task too
-        % easy since subjects can just match color with the key below)
+        % experimental loop --essentially it makes the Stroop task too
+        % easy since subjects can just match color with the key below
         imgBW = rgb2gray(img);  % Convert the image to grayscale
         imageTextureBW = Screen('MakeTexture', window, imgBW);  % Create texture for grayscale image
 
         % Draw the image texture at the calculated position
         Screen('DrawTexture', window, imageTextureBW, [], [imageX, imageY, imageX + imgWidth, imageY + imgHeight]);
 
-        % Check the keyboard. The person should press the
+        % Saves keyboard response
         [keyIsDown,secs, keyCode] = KbCheck;
         if keyCode(escapeKey)
             ShowCursor;
@@ -284,31 +252,23 @@ for trial = 1:numTrials
             respToBeMade = false;
         end
 
-        % Flip to the screen
-        vbl = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi);
+        vbl = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi);  % flips the screen
 
     end
 
-    % Record the reaction time, calculated by the use of flip timestamps
+    % Records reaction time calculated by the use of flip timestamps
     rt = vbl - iEnd;
 
-    % Record the trial data into out data matrix
+    % Records trial data into out data matrix
     respMat(1, trial) = currentID;
     respMat(2, trial) = wordNum;
     respMat(3, trial) = colorNum;
     respMat(4, trial) = response;
-    respMat(5, trial) = rt;
-    
-
-    % we don't use isiRecDuration (actual time between
-    % fixation point and the start of next stimulus) in our demo but we
-    % decided to keep it in since it would be easy to add it to the output
-    % matrix if one were to use it in futher work
-
-    % respMat(1 , trial) = isiRecDuration;
-    
+    respMat(5, trial) = rt; 
 
 end
+
+% 
 
 for ii = 1:9 
     if respMat(3, ii) == respMat(4, ii)
